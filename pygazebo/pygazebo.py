08b04bd6c42302462db1c884caad4c52f5fac763
Ibrahim <ibrahim78786@gmail.com>
2022-04-12 10:52:30 -0500

    Invalid syntax error due to async reserved keyword from python 3.7+;
    Fixed using getattr(asyncio, 'async')

--- pygazebo/pygazebo.py
+++ pygazebo/pygazebo.py
@@ -248,7 +248,9 @@ class _Connection(object):
 
         # TODO jpieper: Either assert that this is numeric, or have a
         # separate DNS resolution stage.
-        future = asyncio.async(loop.sock_connect(self.socket, address))
+        # Error in py 3.9, async is a reserved keyword
+        # fix: https://stackoverflow.com/questions/51196568/create-task-asyncio-async-syntaxerror-invalid-syntax
+        future = getattr(asyncio, 'async')(loop.sock_connect(self.socket, address))
 
         def callback_impl(future):
             try:
@@ -277,7 +279,7 @@ class _Connection(object):
 
     def start_accept(self, callback):
         loop = asyncio.get_event_loop()
-        future = asyncio.async(loop.sock_accept(self.socket))
+        future = getattr(asyncio, 'async')(loop.sock_accept(self.socket))
         future.add_done_callback(
             lambda future: self.handle_accept(callback, future))
 
@@ -292,7 +294,7 @@ class _Connection(object):
         result = asyncio.Future()
 
         loop = asyncio.get_event_loop()
-        future = asyncio.async(loop.sock_recv(self.socket, 8))
+        future = getattr(asyncio, 'async')(loop.sock_recv(self.socket, 8))
         future.add_done_callback(
             lambda future: self.handle_read_raw_header(future, result))
         return result
@@ -324,7 +326,7 @@ class _Connection(object):
                 return
 
             loop = asyncio.get_event_loop()
-            future = asyncio.async(
+            future = getattr(asyncio, 'async')(
                 loop.sock_recv(self.socket,
                                min(total_size - len(starting_data),
                                    self.BUF_SIZE)))
@@ -384,7 +386,7 @@ class _Connection(object):
             next_send = data[to_send:]
 
             loop = asyncio.get_event_loop()
-            future = asyncio.async(loop.sock_sendall(self.socket, this_send))
+            future = getattr(asyncio, 'async')(loop.sock_sendall(self.socket, this_send))
             future.add_done_callback(
                 lambda future: self.send_pieces(next_send, result))
         except Exception as e:
